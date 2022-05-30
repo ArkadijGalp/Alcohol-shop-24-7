ae0ed81bd75d12407808cc218f9c193abff86fd9
Аркадий <arkady.galperin@gmail.com>
2022-05-30 13:49:37 +0300

    Starting to fill shop with goods

---
+++ app/src/main/java/space/arkady/alcoholshop/models/Drink.kt
@@ -0,0 +1,10 @@
+package space.arkady.alcoholshop.models
+
+data class Drink(
+    val productBrandName: String = "",
+    val productDiversity: String = "",
+    val productId: String = "",
+    val productKind: String = "",
+    val alcoholByVolume: String = "",
+    val price: String = ""
+)
